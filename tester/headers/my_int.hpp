0ec804bc06b0c222cc011b0f9259c1ce05b171d3
Steffen Kienzle <skienzle@2-A-3.42heilbronn.de>
2022-06-26 19:52:40 +0200

    removed the casting operator in my_int

--- tester/headers/my_int.hpp
+++ tester/headers/my_int.hpp
@@ -6,31 +6,41 @@
 /*   By: skienzle <skienzle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 11:45:57 by skienzle          #+#    #+#             */
-/*   Updated: 2022/06/03 14:22:34 by skienzle         ###   ########.fr       */
+/*   Updated: 2022/06/26 19:47:04 by skienzle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+#include <iostream>
 
 class my_int
 {
 public:
 	my_int(): _ptr(new int()) {}
 	my_int(int n): _ptr(new int(n)) {}
-	my_int(const my_int& other): _ptr(new int(*other._ptr)) {}
+	my_int(const my_int& other): _ptr(new int(*other)) {}
 
 	~my_int() { delete _ptr; }
-	
-	operator int() const { return *_ptr; }
 
-	bool operator<(const my_int& other) const { return *_ptr < *other._ptr; }
+	int operator*() const { return *_ptr; }
+
+	bool operator==(const my_int& rhs) const
+	{
+		return *(*this) == *rhs;
+	}
+
+	bool operator<(const my_int& rhs) const
+	{
+		return *(*this) < *rhs;
+	}
 
 	my_int& operator=(const my_int& other)
 	{
 		if (this != &other)
 		{
 			delete _ptr;
-			_ptr = new int(*other._ptr);
+			_ptr = new int(*other);
 		}
 		return *this;
 	}
@@ -38,3 +48,11 @@ public:
 private:
 	int *_ptr;
 };
+
+inline std::ostream&
+operator<<(std::ostream& out, const my_int& rhs)
+{
+	out << *rhs;
+	return out;
+}
+
